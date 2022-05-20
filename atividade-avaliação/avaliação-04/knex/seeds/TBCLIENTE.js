7eed82fd489fbdfed4b3cabc30a98988b0d1b0c3
Zion <89410103+ZionGabr1el@users.noreply.github.com>
2022-05-20 13:44:46 -0300

    Add files via upload

---
+++ atividade-avaliação/avaliação-04/knex/seeds/TBCLIENTE.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+
+ exports.seed = function(knex) {
+  return knex('TBCLIENTE').truncate()
+    .then(function () {
+      return knex('TBCLIENTE').insert([
+        {id: 1, codigo: '000', cnpjcpf: '000.000.000-00', nome: "Felipe", TipoCliente: "Pessoa_Fisica"},
+        {id: 2, codigo: '111', cnpjcpf: '111.111.111-11', nome: "Taveira", TipoCliente: "Pessoa_Fisica"},
+        {id: 3, codigo: '222', cnpjcpf: '222.222.222-22', nome: "Zion", TipoCliente: "Pessoa_Fisica"},
+      ]);
+  });
+};
